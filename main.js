fe4518a4e037a8200bbbd1eb9469790216eb3938
Atsuma <asd463291@gmail.com>
2022-05-28 14:21:19 +0800

    Feature: Player creation

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ canvas.height = 576;
 const townImage = new Image();
 townImage.src = "./img/Pellet Town.png";
 
+const playerDownImage = new Image();
+playerDownImage.src = "./img/playerDown.png";
+
 townImage.onload = () => {
     context.drawImage(townImage, -450, -250);
+    playerDownImage.onload = () => {
+        context.drawImage(
+            playerDownImage,
+            0,
+            0,
+            playerDownImage.width / 4,
+            playerDownImage.height,
+            canvas.width / 2 - playerDownImage.width / 4 / 2,
+            canvas.height / 2 - playerDownImage.height / 2,
+            playerDownImage.width / 4,
+            playerDownImage.height
+        );
+    };
 };
