b49ea1fb2831264fa1f92ea352d15da2f0e7245b
Atsuma <asd463291@gmail.com>
2022-05-28 15:03:42 +0800

    Refactor: Extract classes

--- main.js
+++ main.js
@@ -9,22 +9,6 @@ for (let i = 0; i < collisions.length; i += 70) {
     collisionsMap.push(collisions.slice(i, 70 + i));
 }
 
-class Boundary {
-    static width = 48;
-    static height = 48;
-
-    constructor({ position }) {
-        this.position = position;
-        this.width = 48;
-        this.height = 48;
-    }
-
-    draw() {
-        context.fillStyle = "rgba(255,0,0,0)";
-        context.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-}
-
 const boundaries = [];
 const offset = {
     x: -450,
@@ -51,33 +35,6 @@ townImage.src = "./img/Pellet Town.png";
 
 const playerDownImage = new Image();
 playerDownImage.src = "./img/playerDown.png";
-
-class Sprite {
-    constructor({ position, image, frames = { max: 1 } }) {
-        this.position = position;
-        this.image = image;
-        this.frames = frames;
-
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max;
-            this.height = this.image.height;
-        };
-    }
-
-    draw() {
-        context.drawImage(
-            this.image,
-            0,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
-        );
-    }
-}
 
 const player = new Sprite({
     position: {
@@ -111,7 +68,7 @@ const keys = {
     },
 };
 
-const movables = [background, ...boundaries];
+const movables = [background, ...boundaries, foreground];
 
 function rectangularCollision({ rectangle1, rectangle2 }) {
     return (
