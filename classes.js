9e06e574b3f51669fe6bc633775d30d71c748702
Atsuma <asd463291@gmail.com>
2022-05-28 16:47:17 +0800

    Feature: Attacks - Tackle

--- classes.js
+++ classes.js
@@ -1,5 +1,12 @@
 class Sprite {
-    constructor({ position, image, frames = { max: 1, hold: 10 }, sprites, animate = false }) {
+    constructor({
+        position,
+        image,
+        frames = { max: 1, hold: 10 },
+        sprites,
+        animate = false,
+        isEnemy = false,
+    }) {
         this.position = position;
         this.image = image;
         this.frames = { ...frames, val: 0, elasped: 0 };
@@ -11,9 +18,14 @@ class Sprite {
 
         this.animate = animate;
         this.sprites = sprites;
+        this.opacity = 1;
+        this.health = 100;
+        this.isEnemy = isEnemy;
     }
 
     draw() {
+        context.save();
+        context.globalAlpha = this.opacity;
         context.drawImage(
             this.image,
             this.frames.val * this.width,
@@ -25,6 +37,7 @@ class Sprite {
             this.image.width / this.frames.max,
             this.image.height
         );
+        context.restore();
 
         if (!this.animate) return;
 
@@ -36,6 +49,50 @@ class Sprite {
             if (this.frames.val < this.frames.max - 1) this.frames.val++;
             else this.frames.val = 0;
         }
+    }
+
+    attack({ attack, recipient }) {
+        const timeLine = gsap.timeline();
+
+        this.health -= attack.damage;
+
+        let movementDistance = 20;
+        if (this.isEnemy) movementDistance = -20;
+
+        let healthBar = "#enemyHealthBar";
+        if (this.isEnemy) healthBar = "#playerHealthBar";
+
+        timeLine
+            .to(this.position, {
+                x: this.position.x - movementDistance,
+            })
+            .to(this.position, {
+                x: this.position.x + movementDistance * 2,
+                duration: 0.1,
+                onComplete: () => {
+                    // Enemy actually gets hit
+                    gsap.to(healthBar, {
+                        width: this.health + "%",
+                    });
+
+                    gsap.to(recipient.position, {
+                        x: recipient.position.x + 10,
+                        yoyo: true,
+                        repeat: 5,
+                        duration: 0.08,
+                    });
+
+                    gsap.to(recipient, {
+                        opacity: 0,
+                        yoyo: true,
+                        repeat: 5,
+                        duration: 0.08,
+                    });
+                },
+            })
+            .to(this.position, {
+                x: this.position.x,
+            });
     }
 }
 
